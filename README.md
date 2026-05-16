a287f4d300cc7c59e7446222de1f7af01a968463
aidanmaier <maier.aidan@gmail.com>
2026-05-16 17:00:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Zero-config WebSockets connection client by UmerCodez
+
+[https://github.com/UmerCodez/SensorServer/wiki/Connecting-To-the-Server-Using-Service-Discovery]
+
+
+WebSockets server initialised using **Sensor Server** by UmerCodez
+
+GitHub [https://github.com/UmerCodez/SensorServer]
+
+F-Droid [https://f-droid.org/packages/github.umer0586.sensorserver/]
+
+**License GPL-3.0**
---
+++ web_sockets_client.py
@@ -0,0 +1,66 @@
+from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
+import websocket
+import json
+import socket
+
+
+def on_message(ws, message):
+    values = json.loads(message)['values']
+    x = values[0]
+    y = values[1]
+    z = values[2]
+    print("x = ", x , "y = ", y , "z = ", z )
+
+def on_error(ws, error):
+    print("error occurred ", error)
+    
+def on_close(ws, close_code, reason):
+    print("connection closed : ", reason)
+    
+def on_open(ws):
+    print("connected")
+    
+
+def connect(url):
+    ws = websocket.WebSocketApp(url,
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever()
+ 
+
+class MyServiceListener(ServiceListener):
+
+    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        print(f"Service {name} updated")
+
+    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        print(f"Service {name} removed")
+
+    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        info = zc.get_service_info(type_, name)
+        if info:
+            addresses = [socket.inet_ntoa(addr) for addr in info.addresses]
+            print(f"\nService Added:")
+            print(f"  Name: {name}")
+            print(f"  Addresses: {addresses}")
+            print(f"  Port: {info.port}")
+
+            if len(addresses) != 0:
+                address = addresses[0]
+                portNo = info.port
+                print("connecting...")
+                connect(f"ws://{address}:{portNo}/sensor/connect?type=android.sensor.accelerometer")
+                # change type= to access others sensors
+                # type=android.sensor.gyroscope
+
+    
+zeroconf = Zeroconf()
+listener = MyServiceListener()
+browser = ServiceBrowser(zeroconf, "_websocket._tcp.local.", listener)
+try:
+    input("Press enter to exit...\n\n")
+finally:
+    zeroconf.close()
