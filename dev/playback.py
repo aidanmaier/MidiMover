51b6cfeee1008e5a2696da630debeecf31bbae96
aidanmaier <maier.aidan@gmail.com>
2026-07-06 17:46:37 +0100

    updated playback for multi-sensor streaming 	modified:   .gitignore 	modified:   dev/playback.py

--- dev/playback.py
+++ dev/playback.py
@@ -1,23 +1,24 @@
 import pandas as pd
 import time
 import asyncio
+from pathlib import Path
 
 # Input variables
-input_directory = '../data/'
+input_directory = Path(__file__).resolve().parent.parent / 'data'
 input_filename = 'test.csv'
 
 # Output variables
-target_sensor = 'rotation_vector'
-target_axis = 'x'
+target_sensors = ['linear_acceleration', 'rotation_vector']
+target_axes = ['x', 'y', 'z']
 
 class DataLoader():
-    def __init__(self, directory: str, filename: str) -> None:
+    def __init__(self, directory: str | Path, filename: str) -> None:
         """
         Loads captured motion sensor data from .csv to df.
         """
-        self.dir = directory
+        self.dir = Path(directory)
         self.file = filename
-        self.filepath = directory + filename
+        self.filepath = self.dir / filename
 
         # Load data to df from .csv
         df = pd.read_csv(self.filepath, header=[0, 1], index_col=0)
@@ -27,28 +28,34 @@ class DataLoader():
         self.samples = len(df.index) # number of samples
         self.sample_rate = (self.samples - 1) / self.length # in Hz
 
-    def stream(self, sensor: str, axis: str) -> list:
+    def get_sample(self, sensor: str, index: int) -> list:
         """
-        Returns data for given sensor and axis as list.
+        Returns a list of sensor values for all axes of given sensor at given index.
         """
-        data = self.data[(sensor, axis)]
-        return list(data)
+        sample = self.data.iloc[index][sensor]
+        return list(sample)
 
 async def main():
 
     # Load sensor data from .csv
-    data = DataLoader(input_directory, input_filename)
+    sensor_stream = DataLoader(input_directory, input_filename)
 
     # Calculate rates
-    sample_rate = data.sample_rate
+    sample_rate = sensor_stream.sample_rate
     sample_period = 1 / sample_rate # sample period in seconds
-    print(f'\nAverage sample rate: {sample_rate} Hz\n')
+    print(f'\nAverage sample rate: {sample_rate} Hz')
     print(f'Average sample period: {sample_period} seconds\n')
 
     # Stream data at sample rate
     next_time = time.monotonic() # start forward-only clock
-    for sample in data.stream(target_sensor, target_axis):
-        print(sample)
+    index = 0
+    while index < len(sensor_stream.data):
+        
+        # Iterate through target sensors and access data by sample
+        for sensor in target_sensors:
+            print(sensor, sensor_stream.get_sample(sensor, index))
+
+        index += 1
 
         # Normalise loop length to sample_period
         next_time += sample_period # ideal sample period length
